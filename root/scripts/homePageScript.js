00fd7822f6b54dbb906603659c6d3808663bee29
Ania <Ania@Sergeys-MacBook-Air.local>
2018-01-22 16:27:35 -0800

    homePage

--- root/scripts/homePageScript.js
+++ root/scripts/homePageScript.js
@@ -1,7 +1,7 @@
   $(document).ready(function() {
-        $('#circle').circleProgress({
-        value: 0.6
+        $('.circle').circleProgress({
+    value: 0.6
   }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(100 * progress) + '<i>%</i>');
+    $(this).find('strong').html(Math.round(893 * progress) + '<i></i>');
   });
   });
