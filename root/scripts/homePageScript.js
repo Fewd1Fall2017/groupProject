1bd3cd25d166931d18fe39f23ad7b3a2724e6670
Ania <Ania@Sergeys-MacBook-Air.local>
2018-01-22 23:27:29 -0800

    home-page

--- root/scripts/homePageScript.js
+++ root/scripts/homePageScript.js
@@ -1,7 +1,18 @@
   $(document).ready(function() {
-        $('.circle').circleProgress({
-    value: 0.6
+        var waypoint = new Waypoint({
+  element: document.getElementById('trigger'),
+  handler: function() {
+    $('.circle').circleProgress({
+    value: 0.87,
+    size: 200,
+        thickness: 21,
+    fill: {
+      gradient: ["#e5007d", "#ab3e8f", "#5e4e9c"]
+    }
   }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(893 * progress) + '<i></i>');
+    $(this).find('strong').html(Math.round(893 * progress) + '<i></i>');           
   });
+  },
+})
   });
+
